8ffb4fd1eb9cc08d806e28ae397f630fac1d4154
delucc2 <coreyd2108@gmail.com>
2018-05-26 15:00:38 -0400

    make movie info on homepage generic (excluding trailer), make movie manager, and edit movie model

--- home/views.py
+++ home/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
+from info.models import Movie
 
 
-# Create your views here.
+# View for homepage (Schedule tab)
 def index(request):
-    return render(request, 'home/index.html')
+    upcoming_showings = Movie.upcoming_movies
+    context = {'showings': upcoming_showings}
+    return render(request, 'home/index.html', context)
 
 
+# View for Policies and Bylaws tab
 def policies(request):
     return render(request, 'home/policies.html')
 
 
+# View for Requests tab
 def requests(request):
     return render(request, 'home/requests.html')
 
 
+# View for Feedback/FAQ tab
 def feedback(request):
     return render(request, 'home/feedback.html')
 
 
+# View for login page
 def login(request):
     return render(request, 'home/login.html')
--- info/models.py
+++ info/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import date
+from itertools import chain
 
 
 # Model containing bylaws pdf
@@ -28,10 +30,31 @@ class Officers(models.Model):
     projectionists = models.ManyToManyField(Projectionist)
 
 
+# Returns queryset of ONLY upcoming movies
+class UpcomingMovies(models.Manager):
+    def get_queryset(self):
+        return Movie.movies.filter(showing_date__gte=date.today())
+
+
 # Model to represent a movie
 class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    last_shown = models.DateField()
+    title = models.CharField(max_length=200, default="Superbad")
+    showing_date = models.DateField(default=date.today())
+    POSSIBLE_RATINGS = (("G", "G"),
+                        ("PG", "PG"),
+                        ("PG-13", "PG-13"),
+                        ("R", "R"))
+    rating = models.CharField(max_length=5,
+                              choices=POSSIBLE_RATINGS,
+                              default="PG-13")
+    runtime = models.IntegerField(default=0)
+    director = models.CharField(max_length=200, default="John Cena")
+    poster = models.ImageField(upload_to='posters/%Y', default='home/static/home/images/logo.png')
+    trailer_src = models.CharField(max_length=1000, default='https://www.youtube.com/embed/jPEYpryMp2s')
+    description = models.CharField(max_length=10000, default='A moving picture, for sure')
+
+    movies = models.Manager()
+    upcoming_movies = UpcomingMovies()
 
     def __str__(self):
         return self.title
